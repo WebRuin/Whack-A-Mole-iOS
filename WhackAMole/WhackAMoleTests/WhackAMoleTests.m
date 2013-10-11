526c82c4a08b3b82178a703bd5dc30ff8f3919cc
Tim Smith <tim.smith.hdg@gmail.com>
2013-10-10 22:46:25 -0700

    Add rest of files

---
+++ WhackAMole/WhackAMoleTests/WhackAMoleTests.m
@@ -0,0 +1,32 @@
+//
+//  WhackAMoleTests.m
+//  WhackAMoleTests
+//
+//  Created by Timothy P Smith on 11/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "WhackAMoleTests.h"
+
+@implementation WhackAMoleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in WhackAMoleTests");
+}
+
+@end
